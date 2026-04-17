957789193ff8d7ad3afe20d3ebc4259c002e1077
vysakhks-14 <vysakh.ks@root.io>
2026-04-17 18:26:19 +0530

    removed fedora

--- fedora/44-fips/src/test-fips.c
+++
@@ -1,9 +0,0 @@
-#include <wolfssl/options.h>
-#include <wolfssl/wolfcrypt/random.h>
-#include <wolfssl/wolfcrypt/fips_test.h>
-
-int main(void)
-{
-    wc_SetSeed_Cb(wc_GenerateSeed);
-    return wc_RunAllCast_fips();
-}
